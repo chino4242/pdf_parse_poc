e90cbc0ad9dab7ea0ba1fbaeb2a1d2b7874eba52
chino4242 <ryanj.contino@gmail.com>
2025-03-27 16:47:24 -0400

    Gets extract working

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-import pymupdf
+import fitz
 
-doc = pymupdf.open("solid.pdf") # open a document
-out = open("output_solid.txt", "wb") # create a text output
-for page in doc: # iterate the document pages
-    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
-    out.write(text) # write text of page
-    out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
-out.close()
+doc = fitz.open("2024_Rookie_Scouting_Portfolio (1).pdf") # open a document
+out = open("output_solid_RSP_wr.txt", "wb") # create a text output
+start_page = 244
+end_page = 279
+
+with open("output_RSP_wr.txt", "w") as output_file:
+
+ for page_number in range(start_page, end_page):
+        page = doc[page_number]  # Access the page
+        text = page.get_text("text")  # Extract text from the page
+        
+        # Write the text to the output file
+        output_file.write(f"Text from page {page_number + 1}:\n")
+        output_file.write(text)
+        output_file.write("\n\n") 
+doc.close()
