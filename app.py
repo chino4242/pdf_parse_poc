3599ec42d830dd2ad2be07dcc23a1638cdba4aff
chino4242 <ryanj.contino@gmail.com>
2025-03-27 20:22:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import fitz
 
 doc = fitz.open("2024_Rookie_Scouting_Portfolio (1).pdf") # open a document
 out = open("output_solid_RSP_wr.txt", "wb") # create a text output
-start_page = 244
-end_page = 279
+start_page = 189
+end_page = 431
 
 with open("output_RSP_wr.txt", "w") as output_file:
 
